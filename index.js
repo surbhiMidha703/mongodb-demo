82b5c182a18e1af5cc975678bb604498e276bc83
Surbhi Midha <surbhimidha@192.168.20.73>
2022-08-10 12:39:13 +1000

    Connected to mongoDB

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/playground"); // in real world app the connection string should come from config file
+//first time the db is automatically created if that does not exists
+// in real world app the connection string should come from config file
+//return promise
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("connected to mongoDB"))
+  .catch((e) => console.error("could not connect to DB", e));
