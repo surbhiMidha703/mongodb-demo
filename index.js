e92ea87bc461bb03c53f1b8430a34a1457d85403
Surbhi Midha <surbhimidha@192.168.20.73>
2022-08-10 14:31:57 +1000

    Saved document to mongoDB

--- index.js
+++ index.js
@@ -7,3 +7,29 @@ mongoose
   .connect("mongodb://localhost/playground")
   .then(() => console.log("connected to mongoDB"))
   .catch((e) => console.error("could not connect to DB", e));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+// create a class
+const Course = mongoose.model("Course", courseSchema);
+
+//save is an async operation, result will be ready in futire , this returns a promise
+const createCourse = async () => {
+  //create instance (this object maps to a document in a mongoDB)
+  const course = new Course({
+    name: "angular course",
+    author: "surbhi",
+    tags: ["node", "frontend"],
+    isPublished: true,
+  });
+  const result = await course.save();
+  console.log("result=>", result);
+};
+
+createCourse();
